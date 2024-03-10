3f1c17fd9e948b078c40558f9d6f47cdd8981968
glinda <q5481996@gmail.com>
2024-03-10 20:32:56 +0700

    date locale fixed: set US

--- app/src/main/java/ru/glindaqu/pocketjournal_v2_compose/date/DateFormats.kt
+++ app/src/main/java/ru/glindaqu/pocketjournal_v2_compose/date/DateFormats.kt
@@ -2,9 +2,10 @@ package ru.glindaqu.pocketjournal_v2_compose.date
 
 import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
+import java.util.Locale
 
 @SuppressLint("SimpleDateFormat")
-val monthAndDay = SimpleDateFormat("MMMM, dd")
+val monthAndDay = SimpleDateFormat("MMMM, dd", Locale.US)
 @SuppressLint("SimpleDateFormat")
 val dayOnly = SimpleDateFormat("d")
 @SuppressLint("SimpleDateFormat")
